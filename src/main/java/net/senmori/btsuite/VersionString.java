8b7d1e23386b23ddc828fa4d94f5c8d845c76819
Senmori <thesenmori@gmail.com>
2018-07-09 05:27:07 -0400

    Update copyright. Add launch4j compatibility.
    Signed-off-by: Senmori <thesenmori@gmail.com>

--- src/main/java/net/senmori/btsuite/VersionString.java
+++ src/main/java/net/senmori/btsuite/VersionString.java
@@ -1,6 +1,33 @@
-package net.senmori.btsuite;
+/*
+ * Copyright (c) $year, $user. BuildToolsSuite. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * The name of the author may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
 
-import net.senmori.btsuite.storage.BuildToolsSettings;
+package net.senmori.btsuite;
 
 public final class VersionString implements Comparable<VersionString> {
 
@@ -32,7 +59,7 @@ public final class VersionString implements Comparable<VersionString> {
     }
 
     public static boolean isVersionNumber(String versionString) {
-        return "latest".equalsIgnoreCase(versionString) || ( versionString.split("\\.").length > 1 );
+        return ( versionString.split( "\\." ).length > 1 );
     }
 
     public String getAlias() {
@@ -60,10 +87,6 @@ public final class VersionString implements Comparable<VersionString> {
     }
 
     public void parse(String versionString) {
-        if ( versionString.equalsIgnoreCase("latest") ) {
-            parse( BuildToolsSettings.getInstance().getDefaultVersion() ); // parse default version
-            return;
-        }
         String[] arr = versionString.split("\\.");
 
         try {
@@ -91,9 +114,6 @@ public final class VersionString implements Comparable<VersionString> {
 
     @Override
     public int compareTo(VersionString other) {
-        if ( this.versionString.equalsIgnoreCase("latest") )
-            return 1;
-
         if ( this.major != other.major )
             return Integer.compare(this.major, other.major);
 
